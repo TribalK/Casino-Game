9494a18e24e37e64d29c41bd561f0d79120c2e03
Derek Drackley <derekdrackley@gmail.com>
2021-01-30 18:46:59 -0600

    Separate files for each class header

--- Casino Game/Casino Game/Blackjack.cpp
+++ Casino Game/Casino Game/Blackjack.cpp
@@ -1,9 +1,9 @@
-#include "Casino.h"
+#include "Blackjack.h"
 
 //Constructor
 Blackjack::Blackjack(Player &patron)
 {
-	string choice;
+	std::string choice;
 	patron.updateCash();
 
 	if (patron.checkFlags(gameID)) {
@@ -31,7 +31,7 @@ Blackjack::Blackjack(Player &patron)
 	dealer_sum = evaluateDeck(dealerCards);
 
 	if (player_sum == 21) {
-		cout << "What a lucky draw! You got blackjack!\n";
+		std::cout << "What a lucky draw! You got blackjack!\n";
 		patron.setCurrentEarnings(50);
 		patron.setCurrentScore(500);
 		patron.updateCash();
@@ -41,8 +41,8 @@ Blackjack::Blackjack(Player &patron)
 
 	do
 	{
-		cout << "Would you like to Hit or Stay?\n";
-		cin >> choice;
+		std::cout << "Would you like to Hit or Stay?\n";
+		std::cin >> choice;
 		BjCompare(choice);
 
 		if (choice == "hit")
@@ -51,14 +51,14 @@ Blackjack::Blackjack(Player &patron)
 			deck.pop_back();
 			player_sum = evaluateDeck(playerCards);
 			displayDecks(playerCards, dealerCards, playerCards.size(), dealerCards.size());
-			cout << "Current score: " << player_sum << endl;
+			std::cout << "Current score: " << player_sum << std::endl;
 			
 		}
 
 	} while ((choice != "stay") && (player_sum < 21));
 
 	hiddenFlag = false;
-	cout << "The dealer has revealed his down card to be: " << dealerCards.at(0) << endl;
+	std::cout << "The dealer has revealed his down card to be: " << dealerCards.at(0) << std::endl;
 
 	while (dealer_sum < 17)
 	{
@@ -69,8 +69,8 @@ Blackjack::Blackjack(Player &patron)
 
 	displayDecks(playerCards, dealerCards, playerCards.size(), dealerCards.size());
 
-	cout << "You have " << player_sum << " points.\n";
-	cout << "The dealer has " << dealer_sum << " points.\n";
+	std::cout << "You have " << player_sum << " points.\n";
+	std::cout << "The dealer has " << dealer_sum << " points.\n";
 
 	int total = determineWinner(player_sum, dealer_sum);
 
@@ -93,13 +93,13 @@ void Blackjack::shuffle_deck(int n)
 	for (int i = 0; i < n; i++)
 	{
 		int r = i + (rand() % (52 - i));
-		swap(deck[i], deck[r]);
+		std::swap(deck[i], deck[r]);
 	}
 }
 
 void Blackjack::BlackjackHelp()
 {
-	cout << "Here's how Blackjack works. You will receive two cards from the dealer, face-up.\n" <<
+	std::cout << "Here's how Blackjack works. You will receive two cards from the dealer, face-up.\n" <<
 		"The dealer will then give themselves two cards, one face-down and the other face up.\n" <<
 		"Your job is to continue picking up cards by saying 'Hit' until you have either broke\n" <<
 		"the score of 21 or you choose to 'Stay'. The dealer will then continue to hit until\n" <<
@@ -107,7 +107,7 @@ void Blackjack::BlackjackHelp()
 		"If both the player and the dealer are under or within the threshold of 21, the highest score wins.\n\n";
 }
 
-int Blackjack::evaluateDeck(vector<int> cards)
+int Blackjack::evaluateDeck(std::vector<int> cards)
 {
 	int total = 0;
 	int aceCounter = 0;
@@ -126,45 +126,45 @@ int Blackjack::evaluateDeck(vector<int> cards)
 	return total;
 }
 
-string Blackjack::BjCompare(string& choice)
+std::string Blackjack::BjCompare(std::string& choice)
 {
-	transform(choice.begin(), choice.end(), choice.begin(), ::tolower);
+	std::transform(choice.begin(), choice.end(), choice.begin(), ::tolower);
 
 	if (choice.compare("help") == 0)
 		BlackjackHelp();
 
 	else if (choice.compare("hit") != 0 && choice.compare("stay") != 0) {
-		cout << "You entered an incorrect prompt. Please try entering either \"Hit\", \"Stay\", or \"Help\". \n\n";
-		cin.clear();
-		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		std::cout << "You entered an incorrect prompt. Please try entering either \"Hit\", \"Stay\", or \"Help\". \n\n";
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 	}
 
 	return choice;
 }
 
-void Blackjack::displayDecks(vector<int> playerDeck, vector<int> dealerDeck, int pDeckSize, int dDeckSize)
+void Blackjack::displayDecks(std::vector<int> playerDeck, std::vector<int> dealerDeck, int pDeckSize, int dDeckSize)
 {
-	cout << "Player's deck: \n";
+	std::cout << "Player's deck: \n";
 	for (int i = 0; i < pDeckSize; i++)
 	{
-		cout << playerDeck.at(i) << " ";
+		std::cout << playerDeck.at(i) << " ";
 	}
 
-	cout << endl << endl;
-	cout << "Dealer's deck: \n";
+	std::cout << std::endl << std::endl;
+	std::cout << "Dealer's deck: \n";
 
 	//Replacing first character of dealer's deck
 	if (hiddenFlag)
-		cout << "X ";
+		std::cout << "X ";
 	else
-		cout << dealerDeck.at(0) << " ";
+		std::cout << dealerDeck.at(0) << " ";
 
 	for (int i = 1; i < dDeckSize; i++)
 	{
-		cout << dealerDeck.at(i) << " ";
+		std::cout << dealerDeck.at(i) << " ";
 	}
 
-	cout << endl << endl;
+	std::cout << std::endl << std::endl;
 }
 
 int Blackjack::determineBestAces(int n, int total)
@@ -185,27 +185,27 @@ int Blackjack::determineBestAces(int n, int total)
 int Blackjack::determineWinner(int player_sum, int dealer_sum)
 {
 	if (player_sum > 21) {
-		cout << "You broke! The dealer wins!\n";
+		std::cout << "You broke! The dealer wins!\n";
 		return 0;
 	}
 
 	else if (player_sum <= 21 && dealer_sum > 21) {
-		cout << "The dealer broke! You win!\n";
+		std::cout << "The dealer broke! You win!\n";
 		return 30;
 	}
 
 	else {
 		if (player_sum < dealer_sum) {
-			cout << "The dealer wins!\n";
+			std::cout << "The dealer wins!\n";
 			return 0;
 		}
 
 		else if (player_sum > dealer_sum) {
-			cout << "You win!\n";
+			std::cout << "You win!\n";
 			return 50;
 		}
 		else {
-			cout << "You and the dealer tied! You get your original amount back.\n";
+			std::cout << "You and the dealer tied! You get your original amount back.\n";
 			return 10;
 		}
 	}
